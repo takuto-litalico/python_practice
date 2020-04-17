029fcc828eb29ca4501fdbd36cc44da0d953a151
takuto-litalico <60427297+takuto-litalico@users.noreply.github.com>
2020-04-17 18:56:56 +0900

    update practice.py

--- practice.py
+++ practice.py
@@ -6,14 +6,15 @@ seconds = (sec - hour * 3600 - minute * 60)
 
 a_list = [hour, minute, seconds]
 
-def pzero(a):
-    if a <= 10:
-        pa = "0" + str(a)
-    else:
-        pass
-    return pa
+def pzero(num):
+    num_str = str(num)
+    if num < 10:
+        num_str = "0" + str(num)
+    return num_str
 
-for i in a_list:
-    i = pzero(i)
+for i,num in enumerate(a_list):
+    a_list[i] = pzero(num)
+
+hour, minute, seconds = a_list
 
 print(str(hour) + ":" + str(minute) + ":" + str(seconds))
